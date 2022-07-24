b2b279e5c28abc3ce1966c106218a9bc5f8ba548
aliaktas48 <109541007+aliaktas48@users.noreply.github.com>
2022-07-24 13:57:15 +0300

    Add files via upload

--- c_00/ex07/ft_putnbr.c
+++ c_00/ex07/ft_putnbr.c
@@ -6,8 +6,39 @@
 /*   By: aaktas <aaktas@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 19:22:45 by aaktas            #+#    #+#             */
-/*   Updated: 2022/07/23 19:48:54 by aaktas           ###   ########.fr       */
+/*   Updated: 2022/07/24 12:34:35 by aaktas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	char	w;
+
+	if (nb >= 0 && nb < 10)
+	{
+		w = nb + 48;
+		ft_putchar(w);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(nb * -1);
+	}
+	else
+	{	
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
+
+int	main(void)
+{
+	ft_putnbr(42);
+}
